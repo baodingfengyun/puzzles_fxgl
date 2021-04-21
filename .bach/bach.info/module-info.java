de8f1b4b43d769074ce31b3558116f610e5aa300
风云0312 <liupu.mail@gmail.com>
2021-04-21 10:24:53 +0800

    Initial commit

---
+++ .bach/bach.info/module-info.java
@@ -0,0 +1,12 @@
+import com.github.sormuras.bach.ProjectInfo;
+import com.github.sormuras.bach.ProjectInfo.Options;
+import com.github.sormuras.bach.ProjectInfo.Tools;
+
+@ProjectInfo(
+    options = @Options(
+        tools = @Tools(limit = {"javac", "jar", "jlink"})
+    )
+)
+module bach.info {
+  requires com.github.sormuras.bach;
+}
